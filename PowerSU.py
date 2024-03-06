4887f8117871cefe943df5fcbe5724c6b96ddd65
Powerful <53499830+powerful90@users.noreply.github.com>
2024-03-06 09:20:43 -0800

    fix

--- PowerSU.py
+++ PowerSU.py
@@ -5,10 +5,16 @@ import sys
 import argparse
 import subprocess
 import signal
-import colorama
-from colorama import Back, Fore, Style
 
-
+class colors:
+    RED = "\033[91m"
+    GREEN = "\033[92m"
+    YELLOW = "\033[93m"
+    BLUE = "\033[94m"
+    MAGENTA = "\033[95m"
+    CYAN = "\033[96m"
+    WHITE = "\033[97m"
+    RESET = "\033[0m"
 
 class sets:
     set1 = '['
@@ -30,23 +36,23 @@ class sets:
     set17 = 'Status:'
 
 class box:
-    BOX1 = f"{Fore.WHITE}{sets.set1}{Fore.RED}{sets.set8}{Fore.WHITE}{sets.set2}{Fore.RESET}"
-    BOX2 = f"{Fore.WHITE}{sets.set1}{Fore.RED}{sets.set7}{Fore.WHITE}{sets.set2}{Fore.RESET}"
-    BOX3 = f"{Fore.WHITE}{sets.set1}{Fore.GREEN}{sets.set3}{Fore.WHITE}{sets.set2}{Fore.RESET}"
-    BOX4 = f"{Fore.WHITE}{sets.set1}{Fore.GREEN}{sets.set4}{Fore.WHITE}{sets.set2}{Fore.RESET}"
+    BOX1 = f"{colors.WHITE}{sets.set1}{colors.RED}{sets.set8}{colors.WHITE}{sets.set2}{colors.RESET}"
+    BOX2 = f"{colors.WHITE}{sets.set1}{colors.RED}{sets.set7}{colors.WHITE}{sets.set2}{colors.RESET}"
+    BOX3 = f"{colors.WHITE}{sets.set1}{colors.GREEN}{sets.set3}{colors.WHITE}{sets.set2}{colors.RESET}"
+    BOX4 = f"{colors.WHITE}{sets.set1}{colors.GREEN}{sets.set4}{colors.WHITE}{sets.set2}{colors.RESET}"
 
 
 def help():
     print()
-    print(f"{box.BOX3} {Fore.RED}{sets.set12} {Fore.WHITE}{sets.set9} -u {Fore.RED}{sets.set6}{Fore.WHITE}{sets.set11}{Fore.RED}{sets.set5}{Fore.WHITE} -w {Fore.RED}{sets.set6}{Fore.WHITE}{sets.set10}{Fore.RED}{sets.set5}{Fore.RESET}")
+    print(f"{box.BOX3} {colors.RED}{sets.set12} {colors.WHITE}{sets.set9} -u {colors.RED}{sets.set6}{colors.WHITE}{sets.set11}{colors.RED}{sets.set5}{colors.WHITE} -w {colors.RED}{sets.set6}{colors.WHITE}{sets.set10}{colors.RED}{sets.set5}{colors.RESET}")
     print()
 
 def info(username, wordlist):
-    print(f"{box.BOX4} {Fore.WHITE}{sets.set13} {Fore.MAGENTA}{username}{Fore.RESET}")
+    print(f"{box.BOX4} {colors.WHITE}{sets.set13} {colors.MAGENTA}{username}{colors.RESET}")
     os.system("sleep 1")
-    print(f"{box.BOX4} {Fore.WHITE}{sets.set14} {Fore.MAGENTA}{wordlist}{Fore.RESET}")
+    print(f"{box.BOX4} {colors.WHITE}{sets.set14} {colors.MAGENTA}{wordlist}{colors.RESET}")
     os.system("sleep 1")
-    print(f"{box.BOX3} {Fore.WHITE}{sets.set17}{Fore.RESET}")
+    print(f"{box.BOX3} {colors.WHITE}{sets.set17}{colors.RESET}")
     os.system("sleep 1")
 
 def ctrl_c(signal, frame):
@@ -74,7 +80,7 @@ def main():
     for line_number, password in enumerate(lines, start=1):
         password = password.decode('latin-1').strip()  # Decode using a suitable encoding
         progress = line_number * 100 // siz
-        print(f"\033[?25l\r\033[K{Fore.LIGHTWHITE_EX}    {line_number}/{siz}/{progress}%/{password}{Fore.RESET}", end="")
+        print(f"\033[?25l\r\033[K{colors.MAGENTA}    {line_number}/{siz}/{progress}%/{password}{colors.RESET}", end="")
         sys.stdout.flush()
 
         try:
@@ -86,8 +92,8 @@ def main():
             pass
         else:
             # Password successfully decrypted
-            print(f"\n{box.BOX1} {Fore.RED}{sets.set15} {Fore.GREEN}{password} {Fore.RED}{sets.set16} {Fore.GREEN}{line_number}{Fore.RESET}")
-            print(f"{Fore.WHITE}{Fore.RESET}")
+            print(f"\n{box.BOX1} {colors.RED}{sets.set15} {colors.GREEN}{password} {colors.RED}{sets.set16} {colors.GREEN}{line_number}{colors.RESET}")
+            print(f"{colors.WHITE}{colors.RESET}")
             sys.exit(0)
 
 if __name__ == "__main__":
